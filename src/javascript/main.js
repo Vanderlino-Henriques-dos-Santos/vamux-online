01fadc110e54803b2ddb956a1a31bcbd38b578bb
Vanderlino-Henriques-dos-Santos <vanderlinoh@hotmail.com>
2025-06-22 06:43:53 -0300

    Corrigido caminho absoluto do CSS em src/main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,3 +1,3 @@
 // src/main.js
-import '/css/style.css'; // Caminho absoluto a partir da raiz do projeto (que é 'public' para o Vite)
-import './javascript/passageiro.js';
+import '/css/style.css'; // <<-- AQUI! Remova o './' se ele estiver lá.
+import './javascript/passageiro.js';cd 
